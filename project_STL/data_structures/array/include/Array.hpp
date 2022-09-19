7ac4397407e1e4429bddd8ebbd613b7123875634
MichalPustula <michalpustula@gmail.com>
2022-09-19 14:22:02 +0200

    further array implementation, created and tested at() method

--- project_STL/data_structures/array/include/Array.hpp
+++ project_STL/data_structures/array/include/Array.hpp
@@ -1,24 +1,26 @@
 #pragma once
 
+#include <stdexcept>
+
 template<typename T, size_t S>
 class Array
 {
 public:
-    constexpr int size() const
-    {
-        return S;
-    }
+    constexpr size_t size() const { return S; }
+    constexpr size_t max_size() const { return S; }
 
-    T& operator[](size_t index)
-    {
-        return m_Data[index];
-    }
+    constexpr bool empty() const { return false; }
 
-    const T& operator[](size_t index) const
-    {
-        return m_Data[index];
+    T& operator[](size_t index) { return m_data[index]; }
+    const T& operator[](size_t index) const { return m_data[index]; }
+
+    T& at(size_t index) 
+    { 
+        if (index < size()) {return m_data[index];}
+        else {throw (std::out_of_range("Out of range"));}
     }
+    const T& at(size_t index) const { return m_data[index]; }
 
 private:
-    T m_Data[S];
+    T m_data[S];
 };
--- project_STL/data_structures/array/uts/ArrayTests.cpp
+++ project_STL/data_structures/array/uts/ArrayTests.cpp
@@ -1,15 +1,53 @@
 #include "gtest/gtest.h"
 
 #include "../include/Array.hpp"
+#include <vector>
 
-
-
-
-
-TEST(ArrayTests, CreatingArray)
+TEST(ArrayTests, atMethodPastSize)
 {
     Array<int, 5> array;
-    array[2] = 5;
-    EXPECT_EQ(array[2], 5);
+    EXPECT_THROW(array.at(5), std::out_of_range);
 }
+
+TEST(ArrayTests, atMethodWithinBond)
+{
+    Array<int, 5> array{};
+    EXPECT_EQ(array.at(0), 0);
+}
+
+
+TEST(ArrayTests, TestingReadingWithAccesOperator)
+{
+    Array<int, 2> array;
+    array[0] = 4;
+    array[1] = 7;
+
+    const auto& array_ref = array;
+    EXPECT_EQ(array_ref[1], 7);
+}
+
+
+TEST(ArrayTests, CreatingArrayWith5elementsAssigningAndReadingWithAccessOperator)
+{
+    Array<int, 5> array;
+
+    for (size_t i = 0; i < array.size(); i++)
+    {
+        array[i] = i;
+    }
+
+    for (size_t i = 0; i < array.size(); i++)
+    {
+        EXPECT_EQ(array[i], i);
+    }
+}
+
+TEST(ArrayTests, CreatingEmptyArraySetingElementWithAccessOperator)
+{
+    Array<int, 1> array;
+
+    array[0] = 7;
+    EXPECT_EQ(array.size(), 1);
+}
+
 
